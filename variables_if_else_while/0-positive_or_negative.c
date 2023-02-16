24d835389832050e0f5a8bfec1845ff1e5dd31b2
kundi21 <cundi.machado@gmail.com>
2023-02-16 10:15:28 -0800

    variables_if_else_while tasks

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -18,11 +18,11 @@ int main(void)
 	/* your code goes there */
 	if (n > 0)
 	{
-		printf("is positive")
+		printf("is positive");
 	if (n == 0)
-	{	printf("is zero")
+	{	printf("is zero");
 	else
 	{
-		printf("is negative")
+		printf("is negative");
 	return (0);
 }

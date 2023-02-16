ae130743f7258986ac3cc77c3f7e148f54d2eb69
kundi21 <cundi.machado@gmail.com>
2023-02-15 16:57:07 -0800

    tasks hello world kundi

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- *Description: main - Prints out a string to stdout.
- *
- * Return: 0 if success.
- */
+*Description:
+*Return: 0 if success
+*/
 int main(void)
-{	
-puts ("\"Programming is like a multilingual puzzle");
+{
+puts("\"Programming is like a multilingual puzzle");
 return (0);
 }

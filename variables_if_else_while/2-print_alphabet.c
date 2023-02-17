91b18e50ce9370471a202436344ef0a269ee88bf
kundi21 <cundi.machado@gmail.com>
2023-02-17 08:29:10 -0800

    task 2

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,20 +1,16 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 /**
  * main - Prints the alphabet in lowercase
- (*
+ *
  * Return: 0 if success
  */
 int main(void)
 {
-	int (x);
-	char (x == 'a');
-	char (x <= "z\n");
-	char (x++);
-	{
-	putchar(x);
-	}
+	char x = 'a';
+
+	for (; x <= 'z'; x++)
+		putchar(x);
+	putchar('\n');
 	return (0);
 
 }

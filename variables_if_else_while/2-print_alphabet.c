b1db5f421d365cf550ab1c6b22638a3412b17fd0
kundi21 <cundi.machado@gmail.com>
2023-02-17 05:47:37 -0800

    variables_if_else_while tasks

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Prints the alphabet in lowercase
+ (*
+ * Return: 0 if success
+ */
+int main(void)
+{
+	for (int x = 'a'; x <= 'z'; x++)
+	{
+	putchar(x);
+	}
+	return (0);
+
+}

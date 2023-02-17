febf40b7c51d797ea97d6460657cc570ad73dab0
kundi21 <cundi.machado@gmail.com>
2023-02-17 10:26:30 -0800

    variables_if_else_while tasks

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet in lowercase and then upercase
+ *
+ * Return: 0 if success
+ */
+int main(void)
+{
+	char x = 'a';
+	char y = 'A';
+	for (; x <= 'z'; x++)
+	putchar(x);
+	for (; y <= 'Z'; y++)
+	putchar(y);
+	putchar('\n');
+		return (0);
+
+}

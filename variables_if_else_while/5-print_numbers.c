8c12f794c3713a09700b1af98feee40f13f7284d
kundi21 <cundi.machado@gmail.com>
2023-02-18 10:12:38 -0800

    variables_if_else_while tasks

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+/**
+ * main = prints all single digit numbers of base 10 starting from 0.
+ *
+ * Return: if success 0.
+ */
+int main(void)
+{
+	int numero;
+
+	for (numero = 0; numero < 10; numero++)
+
+		printf("%d", numero);
+
+	printf("\n");
+
+	return (0);
+}

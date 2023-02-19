4237ce5ae3082c946882ee025418a7f5b610e898
kundi21 <cundi.machado@gmail.com>
2023-02-19 09:11:41 -0800

    9-times_table.c

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,6 +1,8 @@
 #include "main.h"
 /**
+ * main - check the code for Holberton School students.
  * times_table - function that prints the 9 times table, starting with 0.
+ * Return: Always 0.
  */
 
 void times_table(void)

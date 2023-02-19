b20b475194c7e658f95043b9f8456d440892a263
kundi21 <cundi.machado@gmail.com>
2023-02-19 07:53:36 -0800

    7-print_last_digit.c

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/*
+ * print_last_digit - Returns asolute value
+ * @n: input number
+ * Return: int
+ */
+
+int print_last_digit(int n)
+{
+	int last = n % 10;
+	
+	if (last < 0)
+		last *= -1;
+	_putchar(last + '0');
+	return (last);
+}

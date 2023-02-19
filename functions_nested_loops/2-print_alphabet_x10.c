637d51e0461669bf04a010c2458d264d4973553b
kundi21 <cundi.machado@gmail.com>
2023-02-19 06:58:57 -0800

    task 2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,6 @@
  #include "main.h"
 /**
- * main - check the code.
- *
- * print_alphabet_x10 - function that prints 10 times the alphabet.
- *
- * Return: Always 0.
- *
+ * print_alphabet_x10 - function that prints 10 times the alphabet
  */
 
 void print_alphabet_x10(void)

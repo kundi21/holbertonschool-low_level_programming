341bb7c95fdbc99a595808c66a8f922b62f828db
kundi21 <cundi.machado@gmail.com>
2023-02-24 10:04:23 -0800

    0-isupper.c

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - check the code.
+*
+* int _isupper(int c); -  a function that checks for uppercase character.
+*
+* Returns 1 if c is uppercase
+* Returns 0 otherwise
+ */
+int _isupper(int c);

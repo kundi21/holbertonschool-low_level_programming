0a9824674f1e08f6d20098a33045cdf822f3b6e5
kundi21 <cundi.machado@gmail.com>
2023-02-24 17:54:37 -0800

    tasks231

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/*
+ * print_square - function that prints a square.
+ * @n: Input number
+ */
+void print_square(int n)
+{
+	int j;
+	int k;
+
+	if (n > 0)
+	{
+		for (j = 0; j < n; j++)
+		{
+			for (k = 0; k < n; k++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}

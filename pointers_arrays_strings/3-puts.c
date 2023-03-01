d76c4e6f42d3b76496ab580a56694c1720901500
kundi21 <cundi.machado@gmail.com>
2023-03-01 08:46:46 -0800

    asdasd

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - function that prints a string, followed by a new line, to stdout.
+ *
+ *
+ */
+void _puts(char *str)
+{
+	char str;
+
+	_puts(*str);
+	
+}
+
+		

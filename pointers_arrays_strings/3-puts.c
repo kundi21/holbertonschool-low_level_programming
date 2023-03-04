e9c002f96c7d950308bb5bee7b2427f66c32c00b
kundi21 <cundi.machado@gmail.com>
2023-03-04 09:26:24 -0800

    asdsad

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -9,5 +9,5 @@ void _puts(char *str)
 {
 	for (; *str; str++)
 		_putchar(*str);
-	_putchar('\n');
+	_putchar('\n'#include);
 }
---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * rev_string  - function that prints a string, in reversE.
+ *
+ * @s: pointer to string.
+ */
+void rev_string(char *s)
+{
+    int len = 0;
+    char *start = s;
+    char *end = s;
+
+    while (*end != '\0')
+    {
+        len++;
+        end++;
+    }
+    end--;
+
+    while (start < end)
+    {
+        char tmp = *start;
+        *start = *end;
+        *end = tmp;
+
+        start++;
+        end--;
+    }
+}

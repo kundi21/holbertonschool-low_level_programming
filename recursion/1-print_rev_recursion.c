b823bc730bf2a9748405767e6aca18e77ce1a0e9
kundi21 <cundi.machado@gmail.com>
2023-03-06 09:02:52 -0800

    asdas

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -11,11 +11,11 @@ void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		_putchar('\n');
+		printf(".");
 	}
 	else
 	{
 		_print_rev_recursion(s + 1);
-		_putchar(*s);
+                _putchar(*s);
 	}
 }

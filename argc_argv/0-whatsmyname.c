423c0a804b61e0306850c7f9cc59bd49451faf8d
kundi21 <cundi.machado@gmail.com>
2023-03-09 11:52:08 -0800

    argc_argv

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - main function.
+ *
+ * @argv: argv
+ *
+ * Return: 0 if success.
+ */
+int main(int argc, char *argv[])
+{
+	argc = 0;
+
+	printf("%s\n", argv[argc]);
+	return (0);
+}

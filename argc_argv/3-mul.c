bea66dba0a0848e747b6ff33dc4ebd859497aba6
kundi21 <cundi.machado@gmail.com>
2023-03-12 19:51:27 -0700

    3-mul.c

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main
+ * 
+ * @argc: argc
+ * @argv: argv
+ *
+ * Return: 0 if success.
+ */
+int main(int argc, char **argv)
+{
+	int x, y;
+	
+	if (argc == 3)
+	{
+		x = atoi(argv[1]);
+		y = atoi(argv[2]);
+		printf("%d\n", (x * y));
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	return (0);
+}

58559e3026cba7294589e997adcaae1c170117a4
kundi21 <cundi.machado@gmail.com>
2023-03-20 09:01:26 -0700

    sdaag0

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+ * struct dog - dog data.
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of the dog, but dogs does not have owners.
+ */
 struct dog
 {
 	char *name;

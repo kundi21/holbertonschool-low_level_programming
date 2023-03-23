880ee468602c8e9f007a710e5a8baed4cb8ca7ce
kundi21 <cundi.machado@gmail.com>
2023-03-23 06:28:05 -0700

    asdad

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -6,7 +6,7 @@
  * @age: age of dog
  * @owner: owner of the dog, but dogs does not have owners.
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
@@ -15,5 +15,6 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *, float, char *);
 
 #endif

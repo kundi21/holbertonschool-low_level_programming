9d9391bfbad442e01d2b0e33409a209f0efce67b
kundi21 <cundi.machado@gmail.com>
2023-03-23 06:11:01 -0700

    tasks

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * print_dog - function that prints a struct dog
+ * @d: struct dog.
+ * Return: print_dog
+ */
+void print_dog(struct dog *d)
+{
+	if (!d)
+	{
+		return;
+	}
+
+	printf("Name: %s\n", (d->name != NULL) ? d->name: "(nil)");
+	printf("Age: %6f\n", d->age);
+	printf("Owner: %s\n", (d->owner != NULL) ? d->owner: "(nil)");
+
+}

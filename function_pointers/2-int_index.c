7a20424ca228bedb1d105e8eff66e3fa9addd7b6
kundi21 <cundi.machado@gmail.com>
2023-03-24 11:11:58 -0700

    asdad

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * int_index - function that searches for an integer.
+ * @array: array
+ * @size: size
+ * @cmp: cmp
+ * Return: integer
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	if (array && cmp && size > 0)
+	{
+		int x;
+
+		for (x = 0; x < size; x++)
+		{
+			if (cmp(array[x]) != 0)
+			{
+				return (x);
+			}
+		}
+	}
+}

3adb6fc88f28baba4fe187e891e91cbee015cc21
kundi21 <cundi.machado@gmail.com>
2023-03-24 10:19:32 -0700

    0-print_name.c

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,6 +1,9 @@
 #include "function_pointers.h"
 #include <stdlib.h>
 /**
+ * print_name - function that prints a name.
+ * @name: name
+ * @f: f
  */
 void print_name(char *name, void (*f)(char *))
 {

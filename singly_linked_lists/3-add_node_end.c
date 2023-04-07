b183026049f4d82ff1ab9c71ec0e5de53d102938
kundi21 <cundi.machado@gmail.com>
2023-04-06 19:36:26 -0700

    3-add_node_end.c

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+/**
+ * add_node - add new node at beginning.
+ * @str: str
+ * @head: head
+ * Return: newNode
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *newNode = malloc(sizeof(list_t));
+	list_t *lastNode = *head;
+
+	if (!newNode)
+		return (NULL);
+
+	newNode->str = strdup(str);
+
+	if (!(newNode->str))
+	{
+		free(newNode);
+		return (NULL);
+	}
+
+	newNode->len = strlen(str);
+	if (*head == NULL)
+	{
+		*head = newNode;
+		return (newNode);
+	}
+	while (lastNode->next != NULL)
+	{
+		lastNode = lastNode->next;
+	}
+	lastNode->next = newNode;
+	newNode->next = NULL;
+
+	return (newNode);
+}

fc92377234a4847269e94a7a500724249dec46ed
kundi21 <cundi.machado@gmail.com>
2023-04-06 19:57:09 -0700

    0-print_list.c

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,7 +1,9 @@
 #include "lists.h"
 /**
+ * print_list - print list
+ * @h: h
+ * Return: list
  */
-
 size_t print_list(const list_t *h)
 {
 	int count = 0;
@@ -10,15 +12,13 @@ size_t print_list(const list_t *h)
 		return (0);
 	while (h != NULL)
 	{
-		if(h->str)
-			printf("[%d] %s\n", h -> len, h -> str);
+		if (h->str)
+			printf("[%d] %s\n", h->len, h->str);
 		else
 			printf("[0] (nil)\n");
-		count ++;
-		h = h -> next;
+		count++;
+		h = h->next;
 	}
 
 return (count);
 }
-	
-	

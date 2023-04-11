98344b64abccbefcc2e34c2199b52ebddcd51f0b
kundi21 <cundi.machado@gmail.com>
2023-04-10 19:46:06 -0700

    1-print_binary.c

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_binary - print binary
+ * @n: n
+ */
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		print_binary(n >> 1);
+
+	putchar((n & 1) + '0');
+}

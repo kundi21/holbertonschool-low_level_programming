1ff35be6077968223175b9dea783ae6a5d25c20d
kundi21 <cundi.machado@gmail.com>
2023-04-12 16:44:02 -0700

    main.h

---
+++ file_io/main.h
@@ -0,0 +1,13 @@
+#ifndef FILE_IO_H
+#define FILE_IO_H
+
+#include <stddef.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif

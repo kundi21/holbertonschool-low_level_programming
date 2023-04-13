638daac776cb7eb3c592dd92e550605bb13b7962
kundi21 <cundi.machado@gmail.com>
2023-04-13 12:15:31 -0700

    1-create_file.c

---
+++ file_io/1-create_file.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * create_file - creates a file.
+ * @filename: name of the file
+ * @text_content: text content
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int len = 0;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
+		len = write(fd, text_content, strlen(text_content));
+
+	close(fd);
+	return (len == -1 ? -1 : 1);
+}

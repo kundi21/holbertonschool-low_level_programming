0fd49752e4f1a7ccb579c43f874fc9b87f90252a
kundi21 <cundi.machado@gmail.com>
2023-04-13 19:15:17 -0700

    2-append_text_to_file.c

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -18,8 +18,8 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	if (!text_content)
-		return (-1);
+	if (text_content == NULL)
+		text_content = "";
 
 	len = write(fd, text_content, strlen(text_content));
 

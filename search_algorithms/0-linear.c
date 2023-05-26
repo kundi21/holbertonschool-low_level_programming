dc8b4cb265be7b08b40a3a12c1b5db3394cdaf81
kundi21 <cundi.machado@gmail.com>
2023-05-26 07:08:21 -0700

    0-linear.c

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -15,7 +15,7 @@ int linear_search(int *array, size_t size, int value)
 	if (!array)
 		return (-1);
 
-	for (i ; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		if (array[i] == value)

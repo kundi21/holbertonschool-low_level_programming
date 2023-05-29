a72c83432ddc382d0fb7c6912dfc85a3bf3e355b
kundi21 <cundi.machado@gmail.com>
2023-05-28 18:00:44 -0700

    1-binary.c

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -1,21 +1,40 @@
 #include "search_algos.h"
 /**
- * function that searches for a value in a sorted array of integers
- * using the Binary search algorithm.
- * @array: pointer to the first element of the array to search in 
- * @size: number of elements in array
- * @value: value to search for.
- * Return: 
- */
+ * binary_search - Function that searches for a value
+ * in a sorted array of integers using the Binary search
+ * algorithm
+ * @array: array to be searched
+ * @size: size of the array
+ * @value: value to be finded
+ * Return: the value finded, -1 otherwise.
+*/
 int binary_search(int *array, size_t size, int value)
 {
+	int low = 0;
+	int high = size - 1;
+	int mid;
+	int printer;
 
-	if (!array)
-		return (-1);
-
-	if (array[size / 2] != value)
+	while (low <= high)
 	{
-		printf("Searching in array: %ls", array);
-	} else
-		return (size / 2);
+		mid = low + (high - low) / 2;
+
+		printf("Searching in array: ");
+
+		for (printer = low; printer <= high; printer++)
+		{
+			printf("%d", array[printer]);
+			if (printer < high)
+				printf(", ");
+		}
+		printf("\n");
+
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] < value)
+			low = mid + 1;
+		else
+			high = mid - 1;
+	}
+	return (-1);
 }
